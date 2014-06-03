d13fdfa4d9b660f75c404e849bc96239c1c51a02
Chris Lesch <chrlescUofM@gmail.com>
2014-06-03 10:16:55 -0700

    CreatingANewProject

---
+++ CreatingANewProject/CreatingANewProject/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CreatingANewProject
+//
+//  Created by Chris Lesch on 6/3/14.
+//  Copyright (c) 2014 iD Tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
